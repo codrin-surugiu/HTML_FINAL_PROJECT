865d6d929b7d281a3adfe94f8aec56ae6f484998
codrin-surugiu <49073617+codrin-surugiu@users.noreply.github.com>
2022-05-19 14:24:10 +0300

    "the js code is done"

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const accordionLink = document.querySelectorAll(".accordion-link");
 
-accordionLink.forEach((accordionLink) => {
+accordionLink.forEach((accordionLink, index) => {
   accordionLink.addEventListener("click", () => {
-    if (accordionLink.classList.contains(".active")) {
-      accordionLink.classList.remove(".active");
+    const divCurrentActive = document.querySelector(".active");
+    const divCurrent = document.querySelectorAll(".answer")[index];
+    if (divCurrentActive === divCurrent) {
+      divCurrent.classList.remove("active");
     } else {
-      const accordionActive = document.querySelectorAll(".active");
-      accordionActive.forEach((accordionActive) => {
-        accordionActive.classList.remove(".active");
-      });
-      accordionLink.classList.add(".active");
-  
+      if (!divCurrentActive) {
+        divCurrent.classList.add("active");
+      } else {
+        divCurrentActive.classList.remove("active");
+        divCurrent.classList.add("active");
+      }
     }
   });
 });
